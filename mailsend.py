90619a8e64762342febb7fc935351982a1cb610c
Jacopx <jacopo.nasi@gmail.com>
2017-01-06 10:27:33 +0100

    Updating to python3

--- mailsend.py
+++ mailsend.py
@@ -6,41 +6,40 @@ import getpass
 hs=-1
 ts=-1
 ss=-1
-print 'Choose the send time'
+print ('Choose the send time')
 
 # Choose the send time
 while hs>23 or hs<0:
-    hs = input('Hours: ')
+    hs = int(input('Hours: '))
 while ts>59 or ts<0:
-    ts = input('Minute: ')
+    ts = int(input('Minute: '))
 while ss>59 or ss<0:
-    ss = input('Second: ')
+    ss = int(input('Second: '))
 
-print 'The message will be sent at: %s:%s.%s' % (hs,ts,ss)
+print ('The message will be sent at: {0}:{1}.{2}'.format(hs,ts,ss))
 
 while True:
     try:
-        username = raw_input('Sender mail (omit @gmail.com): ')
-        username = '%s@gmail.com' % (username)
+        username = input('Sender mail (omit @gmail.com): ')
+        username = ('%s@gmail.com' % username)
         password = getpass.getpass('Password for ' + username +  ': ')
-        # password = raw_input('Password for ' + username +  ': ')
         server = smtplib.SMTP('smtp.gmail.com:587')
         server.starttls()
         server.login(username, password)
         break
     except:
-        print 'Wrong login, retry:'
+        print ('Wrong login, retry:')
 
-print '*---------------*'
-print '| Correct login |'
-print '*---------------*'
+print ('*---------------*')
+print ('| Correct login |')
+print ('*---------------*')
 
 # Destination, Subject and Text
-TO = raw_input('Destinator: ')
-SUBJECT = raw_input('Subject: ')
-TEXT = raw_input('Text: ')
+TO = input('Destinator: ')
+SUBJECT = input('Subject: ')
+TEXT = input('Text: ')
 # Merging message
-MSG = """From: %s\nTo: %s\nSubject: %s\n\n%s """ % (username, TO, SUBJECT, TEXT)
+MSG = ("""From: {0}\nTo: {1}\nSubject: {2}\n\n{3}""".format(username, TO, SUBJECT, TEXT))
 
 force=False
 
@@ -54,13 +53,13 @@ while True:
             server.quit()
             issent = datetime.datetime.now()
             delay = issent - hold
-            print '*---------------------------------------------------------------------*'
-            print '| Successfully sent the mail at %s:%s.%s with %s s of delay |' % (isnow.hour, isnow.minute, isnow.second, delay)
-            print '*---------------------------------------------------------------------*\n'
+            print ('*----------------------------------------------------------------------*')
+            print ('| Successfully sent the mail at {0}:{1}.{2} with {3} s of delay |'.format(isnow.hour, isnow.minute, isnow.second, delay))
+            print ('*----------------------------------------------------------------------*\n')
             break
         except: # force mode in case of fail to send
-            print 'Failed to send mail from %s at %s:%s.%s\n FORCE SEND ACTIVATED!' % (username, isnow.hour, isnow.minute, isnow.second)
+            print ('Failed to send mail from {0} at {1}:{2}.{3}\n FORCE SEND ACTIVATED!'.format(username, isnow.hour, isnow.minute, isnow.second))
             force=True
     else: # waiting for the right time
-        print 'Not yet sended, is %s:%s.%s' % (isnow.hour, isnow.minute, isnow.second)
+        print ('Not yet sended, is {0}:{1}.{2}'.format(isnow.hour, isnow.minute, isnow.second))
         time.sleep(0.05)
